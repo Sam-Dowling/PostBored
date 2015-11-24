7ab36bd24e0293bd97935001026cd93b4b72a34b
johnjoelanders <johnjoelanders@outlook.com>
2015-11-24 09:59:30 +0000

    created connection class, created members DAO with template methods

---
+++ PostBored/MembersDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostBored
+{
+    class MembersDAO 
+    {
+
+        string connectionString = PostBored.connection.GetConnection();
+
+        public void SelectMember()
+        {
+
+        }
+
+        public void InsertMember()
+        {
+
+        }
+
+        public void DeleteMember()
+        {
+
+        }
+
+        public void UpdateMember()
+        {
+
+        }
+    }
+}
---
+++ PostBored/connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostBored
+{
+    class connection
+    {
+       static public string GetConnection()
+        {
+            return "Data Source=studentoracle.students.ittralee.ie:1521/orcl;User ID=;Password=";
+        }
+    }
+}
