2df471b98b1b217ff939264247632fb0a67caf79
LukeBluett <LukeBluett@mail.com>
2015-11-24 10:10:52 +0000

    Created Template for DAO's (Posts, Messages, FollowMembers)

---
+++ PostBored/PostsDAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostBored
+{
+    class PostsDAO
+    {
+        string connectionString = PostBored.connection.GetConnection();
+
+        public void SelectMember()
+        {
+
+        }
+
+        public void InsertMember()
+        {
+
+        }
+
+        public void DeleteMember()
+        {
+
+        }
+
+        public void UpdateMember()
+        {
+
+        }
+    }
+}
